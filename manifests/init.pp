71ce3c4ac59f1b184602d2371648777fa7882480
Hannes Doyle <hannes.doyle@ericsson.com>
2018-06-28 13:59:57 +0200

    Configuration with correct syntax...

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,18 @@
 class hiera-hadoop (
-  packages_common        => [ ],
-  hdfs_hostname          => undef,
-  hdfs_hostname2         => undef,
-  yarn_hostname          => undef,
-  yarn_hostname2         => undef,
-  historyserver_hostname => undef,
-  slaves                 => [ ],
-  journalnode_hostnames  => [ ],
-  zookeeper_hostnames    => [ ],
-  cluster_name           => undef,
-  realm                  => undef,
+  $packages_common        = [ ],
+  $hdfs_hostname          = undef,
+  $hdfs_hostname2         = undef,
+  $yarn_hostname          = undef,
+  $yarn_hostname2         = undef,
+  $historyserver_hostname = undef,
+  $slaves                 = [ ],
+  $journalnode_hostnames  = [ ],
+  $zookeeper_hostnames    = [ ],
+  $cluster_name           = undef,
+  $realm                  = undef,
+  $node_type              = undef,
 ) {
   class{ 'hadoop': 
-    packages_common        => $packages_common,
     hdfs_hostname          => $hdfs_hostname,
     hdfs_hostname2         => $hdfs_hostname2,
     yarn_hostname          => $yarn_hostname ,
@@ -23,6 +23,39 @@ class hiera-hadoop (
     zookeeper_hostnames    => $zookeeper_hostnames,
     cluster_name           => $cluster_name,
     realm                  => $realm,
+  }
+
+  if $node_type == 'primary-master' { 
+    include hadoop::namenode
+    include hadoop::resourcemanager
+    include hadoop::historyserver
+    include hadoop::httpfs
+    include hadoop::zkfc
+    include hadoop::journalnode
+
+    class{ 'zookeeper':
+      hostnames => $zookeeper_hostnames,
+      realm     => $realm,
+    }
+  } elsif $node_type == 'secondary-master' {
+    include hadoop::namenode
+    include hadoop::resourcemanager
+    include hadoop::zkfc
+    include hadoop::journalnode
+
+    class{ 'zookeeper':
+      hostnames => $zookeeper_hostnames,
+      realm     => $realm,
+    }
+  } elsif $node_type == 'trinary-master' {
+    include hadoop::journalnode
+
+    class{ 'zookeeper':
+      hostnames => $zookeeper_hostnames,
+      realm     => $realm,
+    }
+  } elsif $node_type == 'frontend' {
+    include hadoop::frontend
   }
 }
 
