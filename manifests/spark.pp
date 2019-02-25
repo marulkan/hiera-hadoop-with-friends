5bebce194862acf64d98ff778e33a551d6046b82
Hannes Doyle <hannes.doyle@ericsson.com>
2019-02-25 12:13:24 +0100

    Bugfix should use module variable instead of local

--- manifests/spark.pp
+++ manifests/spark.pp
@@ -7,13 +7,13 @@ class hiera_hadoop::spark {
   
   include ::spark
 
-  if $node_type == 'primary-master' {
+  if $hiera_hadoop::node_type == 'primary-master' {
     include ::spark::hdfs
     include ::spark::historyserver
     Class['hadoop::namenode::service'] -> Class['::spark::historyserver::service']
-  } elsif $node_type == 'secondary-master' {
+  } elsif $hiera_hadoop::node_type == 'secondary-master' {
     include ::spark::user
-  } elsif $node_type == 'client' {
+  } elsif $hiera_hadoop::node_type == 'client' {
     include ::spark::frontend
   }
 }
