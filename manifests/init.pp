8516b1cc3b247f20a317e9956c39904f95b17f9a
Hannes Doyle <hannes.doyle@ericsson.com>
2019-06-11 16:24:39 +0200

    Kafka superuser config

--- manifests/init.pp
+++ manifests/init.pp
@@ -88,6 +88,8 @@ class hiera_hadoop (
   $kafka_keytab         = '/etc/security/keytab/kafka.service.keytab',
   $kafka_keytab_source  = undef,
 
+  $kafka_properties = {},
+
   $knox_keytab      = '/etc/security/keytab/knox.service.keytab',
   $spark_keytab     = '/etc/security/keytab/spark.service.keytab',
 
